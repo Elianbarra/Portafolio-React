b420baea6f495b95d5a4169a9d9d36471e94fb85
Elianbarra <El.barram@duocuc.cl>
2026-03-07 19:24:09 -0300

    ChangeSpeedBackground

--- src/background-video.js
+++ src/background-video.js
@@ -15,7 +15,7 @@ document.body.addEventListener("mousemove", () => {
   clearTimeout(mouseMoveTimeout);
   mouseMoveTimeout = setTimeout(() => {
     video.pause(); // Pausa el video si el mouse está quieto
-  }, 200); // Pausa después de 2 segundos sin movimiento
+  }, 1); // Pausa después de 2 segundos sin movimiento
 });
 
 // Pausa el video cuando el mouse sale del cuerpo
